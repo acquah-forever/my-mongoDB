6744c9d7cc4dcbc564c77f078f6c78a168406bdf
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-06 17:56:42 +0000

    imported port and mongoose string to .env file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import "dotenv/config"
 import express from "express";
-import NoteModel from "./models/note"
+import NoteModel from "./models/note";
 
 const app = express();
 
 app.get("/", async (req, res) => {
-    const notes = await NoteModel.find().exec()
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
 
 });
 
