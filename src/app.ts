533f23930d24c989913bc4fecba84260f92c8790
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-06 17:19:21 +0000

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import "dotenv/config"
+import express from "express";
+import NoteModel from "./models/note"
+
+const app = express();
+
+app.get("/", (req, res) => {
+    const notes = await NoteModel.find().exec()
+
+});
+
+export default app;
