b1a2b154c017b34944251c14b8abadff2c5dcaa4
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-09 19:38:37 +0000

    fixed mong atlas db ip issues

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import "dotenv/config"
 import express from "express";
-import NoteModel from "./models/note";
+import Note from "./models/note";
 
 const app = express();
 
 app.get("/", async (req, res) => {
-    const notes = await NoteModel.find().exec();
+    const notes = await Note.find().exec();
     res.status(200).json(notes);
 
 });
