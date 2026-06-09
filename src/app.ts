c2f7d88d9af820b226ad7345477f4fc50284259f
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-09 20:00:04 +0000

    added error handling status 500

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,15 @@ import Note from "./models/note";
 const app = express();
 
 app.get("/", async (req, res) => {
-    const notes = await Note.find().exec();
-    res.status(200).json(notes);
-
+    try {
+        const notes = await Note.find().exec();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error(error);
+        let errorMessage = "An unknown error occured";
+        if(error instanceof Error) errorMessage = error.message;
+        res.status(500).json({error:errorMessage})
+    }
 });
 
 export default app;   
