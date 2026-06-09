b2b1efe1be216ea269301f73113b9a0325e763ed
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-09 20:19:30 +0000

    moved error handling to app.use

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,26 @@
 import "dotenv/config"
-import express from "express";
+import express, { NextFunction, Request } from "express";
 import Note from "./models/note";
 
 const app = express();
 
 app.get("/", async (req, res) => {
     try {
+        throw Error("trums")
         const notes = await Note.find().exec();
         res.status(200).json(notes);
     } catch (error) {
-        console.error(error);
-        let errorMessage = "An unknown error occured";
-        if(error instanceof Error) errorMessage = error.message;
-        res.status(500).json({error:errorMessage})
     }
+});
+
+// error handler: define typescript for express error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occured";
+    if (error instanceof Error) errorMessage = error.message;
+    res.status(500).json({ error: errorMessage })
+
 });
 
 export default app;   
