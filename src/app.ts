76211407494a7078cc05e93776503aeeb7644778
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-13 21:01:38 +0000

    configured postman

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import router from './routes/notes';
 
 const app = express();
 
-// for sending and receiving json data to our server
+// for sending and receiving json data on server
 app.use(express.json());
 
 // main endpoint
