ca159ad4b89767754198b054d3940286dd970849
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-15 15:32:21 +0000

    installed tanstack and browser router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route} from "react-router-dom"
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
     <div>
-     
+      <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route /> 
+      </Routes>
+    </QueryClientProvider>
     </div>
   )
 }
