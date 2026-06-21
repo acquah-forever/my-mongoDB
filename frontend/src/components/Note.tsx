0e5efb0cd83b8c1c2ffa6aea7b8cd2b492448cf9
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-21 13:08:11 +0000

    created note interface and exported

---
+++ frontend/src/components/Note.tsx
@@ -0,0 +1,15 @@
+import type { Note } from '../models/note'
+
+interface NoteProps {
+    note: Note
+};
+
+const Note = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Note;
