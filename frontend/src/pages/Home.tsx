bcf520fd332f6c7a8bbf6efcc68ba6d4e5e1a7a6
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-22 15:25:25 +0000

    tested PATCH from both frontend and postman

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -24,13 +24,13 @@ const Home = () => {
             {isLoading && <p>Loading...</p>}
             {isError && <p>Something went wrong</p>}
 
-            <ul className='max-w-3xl grid grid-cols-2 m-3 gap-5'>
+            <ul className='max-w-3xl grid grid-cols-1 sm:grid-cols-2 m-3 gap-5'>
                 {notes?.map((note) => (
-                    <li className='border border-slate-500 p-4' key={note._id}>
+                    <li className='bg-amber-100 border border-slate-500 p-4' key={note._id}>
                         <h3 className='text-2xl font-semibold'>{note.title}</h3>
                         <p className='text-lg'>{note.text}</p>
-                        <div className='border border-slate-300 mt-7 mb-7'></div>
-                        <p className='text-sm'>{note.createdAt}</p>
+                        <div className='border border-slate-400 mt-7 mb-7'></div>
+                        <p className='text-sm'>{note.updatedAt}</p>
 
                         <br />
                     </li>
