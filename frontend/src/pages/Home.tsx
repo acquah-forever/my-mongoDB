560fee0dd7186708ce3b3c5502558688d701b24d
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-06-26 23:26:19 +0000

    hi to h2

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -52,7 +52,7 @@ const Home = () => {
             <ul className='max-w-3xl grid grid-cols-1 sm:grid-cols-2 m-3 gap-5'>
                 {notes?.map((note) => (
                     <li className='bg-amber-100 border border-slate-500 p-4' key={note._id}>
-                        <h3 className='text-2xl font-semibold'>{note.title}</h3>
+                        <h2 className='text-2xl font-semibold'>{note.title}</h2>
                         <p className='text-lg'>{note.text}</p>
                         <div className='border border-slate-400 mt-7 mb-7'></div>
                         {/* <p className='text-sm'>{createdUpdatedText}</p> */}
