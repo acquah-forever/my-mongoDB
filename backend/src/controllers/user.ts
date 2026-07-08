ea3f382e54cb5785ef35cac5958a2d65654740e6
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-07-08 19:59:25 +0000

    installed bcrypt for hash password protection

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import createHttpError from "http-errors";
+import UserModel from "../models/user";
+import bcrypt from "bcrypt";
 
 interface SignUpBody {
     username?: string;
@@ -16,6 +18,29 @@ export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = asy
         if (!username || !email || !passwordRaw) {
             throw createHttpError(400, "Parameters missing");
         }
+
+        const existingUsername = await UserModel.findOne({ username: username }).exec();
+
+        if (existingUsername) {
+            throw createHttpError(409, "Username already exists");
+        }
+
+        const existingEmail = await UserModel.findOne({ email: email }).exec();
+
+        if (existingEmail) {
+            throw createHttpError(409, "Email already exists");
+        }
+
+        const passwordHashed = await bcrypt.hash(passwordRaw, 10);
+
+        const newUser = await UserModel.create({
+            username: username,
+            email: email,
+            password: passwordHashed,
+        });
+
+        res.status(201).json(newUser);
+
     } catch (error) {
         next(error);
     }
