1a8b7fee73100b2d7fdbdb55caed4bf5067764bf
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-07-15 23:27:22 +0000

    added get user, signup, login and logout

--- frontend/src/services/services.ts
+++ frontend/src/services/services.ts
@@ -3,7 +3,12 @@ import type { User } from "../models/user";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getUser(): Promise<User> {
-    const res = await fetch(`${API_URL}/user`);
+    const res = await fetch(`${API_URL}/users/me`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+    });
     if (!res.ok) {
         throw new Error("Failed to fetch user");
     }
@@ -17,7 +22,7 @@ type signUpCredentials = {
 }
 
 export async function signUp(credentials: signUpCredentials): Promise<User> {
-    const res = await fetch(`${API_URL}/auth/signup`, {
+    const res = await fetch(`${API_URL}/users/signup`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -31,3 +36,36 @@ export async function signUp(credentials: signUpCredentials): Promise<User> {
 
     return res.json() as Promise<User>;
 }
+
+type loginCredentials = {
+        email: string;
+        password: string;
+    }
+
+export async function login(credentials: loginCredentials): Promise<User> {
+    const res = await fetch(`${API_URL}/users/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(credentials)
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to log in");
+    }
+
+    return res.json() as Promise<User>;
+}
+
+
+async function logoutUser() {
+        const res = await fetch("/api/users/logout", {
+            method: "POST",
+        })
+
+        if (!res.ok) {
+            throw new Error("Failed to log out user")
+        }
+
+    }
